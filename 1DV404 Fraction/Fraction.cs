0a31264b1343af158a5d31fa8b08117bb5a8ec4c
Henrik Larsson <hl222mn@student.lnu.se>
2014-11-24 20:50:57 +0100

    1DV404-Fraction

---
+++ 1DV404 Fraction/Fraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV404_Fraction
+{
+    public class Fraction
+    {
+        //skapa en klass...
+
+        //T och N är heltal
+        Fraction F = new Fraction();
+
+        // om N är 0 ska felmeddelande lämnas
+
+        //Följande medlemmar
+        //konstruktor skapar och init nytt bråktal
+        //met oderna getNUmerator och getDenominator som returnerar täljaren resp nämnaren.
+        //met isNegative som ger true om det är neg heltal
+        //met add och multiply som utför bråktalsoperation på två bråk och returnerar ett nytt (hantera 0?)
+        //isEqualTo som jämför två Fractoin.instanser och ser om de representerar samma bråkltal
+        // toString som returnerar en strängrepresentation av brktalet på form T/N
+
+        //Ev fler metoder som kanske fattas???
+        //LÄmpliga argument- och returtyper???
+    }
+}

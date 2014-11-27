d9b5daba3f46059036378b3ae2aa31a08612b6d2
Henrik Larsson <hl222mn@student.lnu.se>
2014-11-27 16:18:18 +0100

    1DV404-Fraction

--- 1DV404 Fraction/Fraction.cs
+++ 1DV404 Fraction/Fraction.cs
@@ -5,23 +5,98 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _1DV404_Fraction
-{
-    public class Fraction
+{ 
+    public class Fraction //skapa en klass...
     {
-        //skapa en klass...
-
         //T och N är heltal
-        Fraction F = new Fraction();
+        int numerator;
+        int denominator;
+
+         //Följande medlemmar
+        //konstruktor skapar och init nytt bråktal
+        public Fraction myFraction = new Fraction(); //http://msdn.microsoft.com/en-us/library/s53ehcz3.aspx
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
 
         // om N är 0 ska felmeddelande lämnas
+        if (numerator = 0)
+            {
+             throw new FormatException("Felaktigt värde - nämnaren kan inte vara 0 i ett bråktal");
+            }
+        }   
+    
 
-        //Följande medlemmar
-        //konstruktor skapar och init nytt bråktal
-        //met oderna getNUmerator och getDenominator som returnerar täljaren resp nämnaren.
-        //met isNegative som ger true om det är neg heltal
+        //metoderna getNUmerator och getDenominator som returnerar täljaren resp nämnaren.
+    public int getNumerator()
+    {
+        get 
+        {
+            return getNumerator;
+        }
+        set
+        {
+            numerator = value; 
+        }
+    }
+    public int getDenominator()
+    {
+        get
+        {
+            return getDenominator;
+        }
+            set
+        {
+            denominator = value;
+        }
+    }
+
+        //metoden isNegative som ger true om det är neg heltal
+
+public bool isNegative()
+    {
+    if (getNumerator < 0 || getDenominator < 0)
+        {
+            return true;
+        }
+    else 
+        {
+            return false; 
+        }
+
+    }
         //met add och multiply som utför bråktalsoperation på två bråk och returnerar ett nytt (hantera 0?)
-        //isEqualTo som jämför två Fractoin.instanser och ser om de representerar samma bråkltal
-        // toString som returnerar en strängrepresentation av brktalet på form T/N
+public static multiply *(Fraction a, Fraction b)
+{
+    return new multiply(a.numerator * b.denominator + b.numerator * a.denominator, a.denominator * b.denominator)
+}
+
+public static add +(Fraction a, Fraction b)
+{
+    
+}
+
+
+
+        //isEqualTo som jämför två Fraction.instanser och ser om de representerar samma bråkltal
+public static bool isEqualTo()
+    {
+    if (getNumerator == getNumerator)
+        {
+        return true;
+        }
+    else
+        {
+        return false; 
+        }
+    }
+
+
+        // toString som returnerar en strängrepresentation av bråktalet på form T/N
+public string toString()
+{
+    return string ("{0}/{1}", getNumerator, getDenominator)
+}
 
         //Ev fler metoder som kanske fattas???
         //LÄmpliga argument- och returtyper???
